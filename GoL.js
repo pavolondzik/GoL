4d5c09bc9b0593f85fec97e84256bdac77c00481
Pavol Ondzik <pavol.ondzik@gmail.com>
2013-10-27 00:42:40 +0100

    On branch CleanWebsite
    Changes to be committed:
    	modified:   GoL.html
    	modified:   GoL.js
    	new file:   GoL.sln
    	new file:   GoL.v11.suo
    	new file:   Helpers.js
    	new file:   Web.config
    
    Updates
    	Site layout deleted. Grid was created using rectangles.
    	Introduced Helpers javascript file for auxiliary functions.
    	Used random function to seed the grid.

--- GoL.js
+++ GoL.js
@@ -1,41 +1,6 @@
 /* (Global) Variables */
-var header = document.getElementById("Header");
-var container = document.getElementById("Container");
 var solution = document.getElementById("Solution");
-var task =  document.getElementById("Task");
-
-/* Website content */
-// ElementId = element in which new element will be created
-function Msg(ElementId, newElementTag, Message)
-{
-    if (typeof ElementId == 'string' || ElementId instanceof String)
-        var Element = document.getElementById(ElementId);
-    else if (typeof ElementId === 'object')
-        var Element = ElementId;
-    if (Element === undefined || Element == null || Element.length <= 0)
-        return;
-
-    if (newElementTag === undefined || newElementTag == null || newElementTag.length <= 0)
-    {
-        newElement = document.createElement("p");
-    }
-    else
-    {
-        newElement = document.createElement(newElementTag);
-    }
-    newElement.innerHTML = Message;
-    Element.appendChild(newElement);
-}
-
-document.title = "Game of Life";
-
-Msg("Header", "h1", "Game of Life");
-
-Msg("Task", "h2", "Task");
-Msg("Task", "h3", "Emergence: Cellular Automata");
-Msg("Task", "", "Create an interactive implementation of the Game of Life grid, where a player can select starting points for a variety of blinkers. The system then sends gliders to destroy them, resulting in chaos. Alternatively, system randomly sets some blinkers and player sets starting point for gliders. Documentation should clearly explain algorithms used to generate the life-forms.");
-
-Msg("Solution", "h2" , "Solution");
+var output = document.getElementById("Output");
 
 var canvas = document.createElement("canvas");
 solution.appendChild(canvas);
@@ -43,61 +8,49 @@ solution.appendChild(canvas);
 /* Website styles */
 document.bgColor = "#C2C2A3";
 
-// Styles for "Header"
-header.style.backgroundColor = "#D4C7A3";
-header.style.padding = "0px 0px 0px 10px";
+/************************/
+/* Solution of the task */
+/************************/
 
-// Styles for main "Container"
-container.style.width = "1000px";
-container.style.margin = "0 auto";
+var context=canvas.getContext("2d");
+//context.scale(0.5, 0.5);
 
-// Styles for "Solution"
-solution.style.backgroundColor = "#669999";
-solution.style.cssFloat = "left";
-solution.style.height = "300px";
-solution.style.padding = "0px 0px 0px 10px";
-solution.style.width = "56%";
-
-// Styles for "Task"
-task.style.backgroundColor = "#f3f3f3";
-task.style.width = "40%";
-task.style.height = "300px";
-task.style.cssFloat = "right";
-task.style.padding = "20px 15px 20px 10px";
+var cellSize = 10; //pixels
+var cellsX = 100;
+var cellsY = 50;
 
 // Styles for "Canvas"
 canvas.style.backgroundColor = "White";
-canvas.style.border = "1px solid #000000";
-canvas.width = 500;
-canvas.height = 300;
+canvas.width = cellsX * cellSize;
+canvas.height = cellsY * cellSize;
 
-/*----------------------*/
-/* Solution of the task */
-var context=canvas.getContext("2d");
-context.scale(0.5, 0.5);
+onColour = 'rgb(0, 0, 0)',
+offColour = 'rgb(255, 255, 255)',
+gridColour = 'rgb(50, 50, 50)';
 
-var p = 10; // cell size ?
-var CellsX = 100;
-var CellsY = 50;
 
-function drawBoard()
-{
-    // Vertical lines
-    for (var x = 0; x <= canvas.width; x += 20) {
-        context.moveTo(0.5 + x + p, p);
-        context.lineTo(0.5 + x + p, canvas.height + p);
-    }
-
-    // Horizontal lines
-    for (var x = 0; x <= canvas.height; x += 20) {
-        context.moveTo(p, 0.5 + x + p);
-        context.lineTo(canvas.width + p, 0.5 + x + p);
-    }
-
-    context.strokeStyle = "black";
+function drawCell(x, y, alive)
+{    
+    context.beginPath();
+    context.rect(x * cellSize, y * cellSize, cellSize, cellSize);
+    context.fillStyle = (alive) ? onColour : offColour;
+    context.fill();
+    context.lineWidth = 1;
+    context.strokeStyle = 'black';
     context.stroke();
 }
 
-drawBoard();
-//var str = "";
-//Msg("Solution", "output" , str.concat(canvas.width," x ", canvas.height));
+function paint() {
+    var x,
+        y;
+
+    for (x = 0; x < cellsX; x++) {
+        for (y = 0; y < cellsY; y++) {
+            drawCell(x, y, random());
+        }
+    }
+}
+
+paint();
+
+output.innerHTML = canvas.width.toString().concat(" x ", canvas.height);
---
+++ Helpers.js
@@ -0,0 +1,12 @@
+﻿// dont need this now
+//function concat(string1, string2)
+//{
+//    if (typeof string1 == "string" || string1 instanceof String)
+//        if (typeof string2 == "string" || string2 instanceof String)
+//            return string1.concat(string2);
+//}
+
+function random()
+{
+    return Math.random() < 0.5 ? true : false;
+}
