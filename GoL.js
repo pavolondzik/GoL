2b54218857e338cedffef74b9f6dc6f9d12537cb
Pavol Ondzik <pavol.ondzik@gmail.com>
2013-10-27 10:53:31 +0000

    On branch CleanWebsite
     Changes to be committed:
    	modified:   GoL.js
    	modified:   GoL.v11.suo
    	modified:   Helpers.js
    
    Description
    	New display mode introduced using enumerate types. Cell's life is displayed
    	with black square (in mode RECT) or with blue circle (in mode CIRCLE).

--- GoL.js
+++ GoL.js
@@ -1,4 +1,5 @@
 /* (Global) Variables */
+var message = "";
 var solution = document.getElementById("Solution");
 var output = document.getElementById("Output");
 
@@ -13,7 +14,6 @@ document.bgColor = "#C2C2A3";
 /************************/
 
 var context=canvas.getContext("2d");
-//context.scale(0.5, 0.5);
 
 var cellSize = 10; //pixels
 var cellsX = 100;
@@ -24,33 +24,62 @@ canvas.style.backgroundColor = "White";
 canvas.width = cellsX * cellSize;
 canvas.height = cellsY * cellSize;
 
-onColour = 'rgb(0, 0, 0)',
-offColour = 'rgb(255, 255, 255)',
-gridColour = 'rgb(50, 50, 50)';
+onColour = 'black',
+onColourCircle = 'blue',
+offColour = 'white',
+gridColour = 'rgb(50, 50, 50)'; //dark grey
+
+ModeEnum = {
+    RECT: 0,
+    CIRCLE: 1
+}
 
 
-function drawCell(x, y, alive)
-{    
-    context.beginPath();
-    context.rect(x * cellSize, y * cellSize, cellSize, cellSize);
-    context.fillStyle = (alive) ? onColour : offColour;
-    context.fill();
-    context.lineWidth = 1;
-    context.strokeStyle = 'black';
-    context.stroke();
+function drawCell(x, y, alive, displayMode)
+{
+    switch(displayMode)
+    {
+        case ModeEnum.RECT:
+            context.beginPath();
+            context.rect(x * cellSize, y * cellSize, cellSize, cellSize);
+            context.fillStyle = (alive) ? onColour : offColour;
+            context.fill();
+            context.lineWidth = 1;
+            context.strokeStyle = gridColour;
+            context.stroke();
+            break;
+        case ModeEnum.CIRCLE:
+            context.beginPath();
+            context.rect(x * cellSize, y * cellSize, cellSize, cellSize);
+            context.fillStyle = offColour;
+            context.fill();
+            context.lineWidth = 1;
+            context.strokeStyle = gridColour;
+            context.stroke();
+            if (alive) {
+                context.beginPath();
+                context.arc((x * cellSize) + 5, (y * cellSize) + 5, 3, 0, 2 * Math.PI);
+                context.fillStyle = (alive) ? onColourCircle : offColour;
+                context.fill();
+                context.stroke();
+            }
+            break;
+    }
 }
 
 function paint() {
     var x,
         y;
 
-    for (x = 0; x < cellsX; x++) {
-        for (y = 0; y < cellsY; y++) {
-            drawCell(x, y, random());
+    for (x = 0; x < cellsX; x++)
+    {
+        for (y = 0; y < cellsY; y++)
+        {
+            drawCell(x, y, random(), ModeEnum.RECT);
         }
     }
 }
-
 paint();
-
-output.innerHTML = canvas.width.toString().concat(" x ", canvas.height);
+message = "Resolution: " + canvas.width.toString() + "x" + canvas.height.toString() + ", ";
+message += "Cells: " + cellsX + "x" + cellsY;
+output.innerHTML = message;
--- Helpers.js
+++ Helpers.js
@@ -1,12 +1,4 @@
-﻿// dont need this now
-//function concat(string1, string2)
-//{
-//    if (typeof string1 == "string" || string1 instanceof String)
-//        if (typeof string2 == "string" || string2 instanceof String)
-//            return string1.concat(string2);
-//}
-
-function random()
+﻿function random()
 {
     return Math.random() < 0.5 ? true : false;
 }
