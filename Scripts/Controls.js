359b34c8e792b0eec146050671238bf8318f1877
Pavol Ondzik <pavol.ondzik@gmail.com>
2013-12-19 17:32:32 +0000

    Ver 2.1   On branch gh-pages   Changes to be committed:  	modified:   Content/Style.css  	modified:   Scripts/Controls.js  	modified:   Scripts/GoL.js  	modified:   index.html

--- Scripts/Controls.js
+++ Scripts/Controls.js
@@ -1,24 +1,82 @@
-﻿// Check DOM state; if ready(complete), run initUniverse
-var cellsX;
-var cellsY;
-
-$('document').ready(function () {
+﻿$(document).ready(function () {
+    var start_value = 'Start Life';
+    var stop_value = 'Stop Life';
     
     // Creating tabs
     $("#tabs").tabs();
     
     // Setting Universe dimensions
-    var spinnerX = $("#spinnerX").spinner();
-    var spinnerY = $("#spinnerY").spinner();
+    var spinnerX = $("#spinnerX").spinner({ min: 20, max: 100 }).val(80);
+    var spinnerY = $("#spinnerY").spinner({ min: 20, max: 100 }).val(40);
 
-    spinnerX.spinner({ min: 4, max: 100 }).val(80);
-    spinnerY.spinner({ min: 4, max: 50 }).val(40);
+    var SpinnerCellsX = parseInt(spinnerX.val(),10);
+    var SpinnerCellsY = parseInt(spinnerY.val(),10);
 
-    cellsX = spinnerX.spinner('value');
-    cellsY = spinnerY.spinner('value');
+    // Initializing universe with dimensions
+    Life.initUniverse(SpinnerCellsY, SpinnerCellsX);
 
-    Life.initUniverse(cellsY, cellsX);
+/* START GRAPHICS > SIZE > SPINNER */
+    spinnerX.spinner({
+        spin: function (event, ui) {
+            SpinnerCellsX = parseInt(ui.value,10);
+            Life.initUniverse(SpinnerCellsY, SpinnerCellsX);
+        }
+    });
+    spinnerX.on("keypress", function (event, ui) {
+        var code = event.keyCode || event.which;
+        var val = parseInt($(this).spinner('value'),10);
+        var min = parseInt($(this).spinner("option", "min"),10);
+        var max = parseInt($(this).spinner("option", "max"),10);
+        if (min <= val && max >= val) {
+            SpinnerCellsX = val;
+            if (code === 13) {
+                $(this).spinner('value', SpinnerCellsX);
+                Life.initUniverse(SpinnerCellsY, SpinnerCellsX);
+            }
+        }
+    });
 
+    spinnerY.spinner({
+        spin: function (event, ui) {
+            SpinnerCellsY = parseInt(ui.value,10);
+            Life.initUniverse(SpinnerCellsY, SpinnerCellsX);
+        }
+    });
+    spinnerY.on("keypress", function (event, ui) {
+        var code = event.keyCode || event.which;
+        var val = parseInt($(this).spinner('value'),10);
+        var min = parseInt($(this).spinner("option", "min"),10);
+        var max = parseInt($(this).spinner("option", "max"),10);
+        if (min <= val && max >= val) {
+            SpinnerCellsY = val;
+            if (code === 13) {
+                $(this).spinner('value', SpinnerCellsY);
+                Life.initUniverse(SpinnerCellsY, SpinnerCellsX);
+            }
+        }
+    });
+/* STOP GRAPHICS > SIZE > SPINNER */
+
+/* START HOME > CONTROLS */
+    $('#start').click(function () {
+        var text = $(this).text();
+        if (text === start_value)
+            $(this).text(stop_value);
+        else $(this).text(start_value);
+        Life.toggleLife();
+    });
+
+    $('#nextGen').click(function () {
+        Life.nextGeneration()
+    });
+
+    $('#clear').click(function () {
+        Life.clearUniverse();
+    });
+/* STOP HOME > CONTROLS */
+
+/* START HOME > PATTERNS */
+    // Loading pattern
     $('#patterns').change(function () {
         $('#patterns option:selected').each(function () {
             url = $(this).val();
@@ -27,98 +85,97 @@ $('document').ready(function () {
             }
         });
     });
+/* STOP HOME > PATTERNS */
 
-});
-
-$("#spinnerX").spinner({
-    spin: function (event, ui) {
-        cellsX = $(this).spinner('value');
-        Life.initUniverse(cellsY, cellsX);
-    }
-});
-
-$("#spinnerY").spinner({
-    spin: function (event, ui) {
-        cellsY = $(this).spinner('value');
-        Life.initUniverse(cellsY, cellsX);
-    }
-});
-
-var start_value = 'Start Life';
-var stop_value = 'Stop Life';
-var startButton = document.getElementById('start');
-var minusButton = document.getElementById('minus');
-var plusButton = document.getElementById('plus');
-var nextGenButton = document.getElementById('nextGen');
-var clearUniverseButton = document.getElementById('clear');
-var displayModeSelect = document.getElementById('displayMode');
-var automaticRadioButton = document.getElementById("automaticRadioBtn");
-
-startButton.onclick = function () {
-    if (startButton.innerHTML === start_value) {
-        startButton.innerHTML = stop_value;
-    } else {
-        startButton.innerHTML = start_value;
-    }
-    Life.toggleLife();
-};
-
-minusButton.onclick = function () {
-    Life.changeSpeed(false);
-    document.getElementById('speed').innerHTML = 100 - (Life.speed / 10);
-};
-
-plusButton.onclick = function () {
-    Life.changeSpeed(true);
-    document.getElementById('speed').innerHTML = 100 - (Life.speed / 10);
-};
-
-nextGenButton.onclick = function () {
-    Life.nextGen();
-};
-
-clearUniverseButton.onclick = function () {
-    Life.clearUniverse();
-};
-
-displayModeSelect.onchange = function () {
-    var index = displayModeSelect.selectedIndex;
-    var options = displayModeSelect.options;
-    var mode = ModeEnum.RECT;
-    switch (options[index].value) {
-        case 'RECT':
-            mode = ModeEnum.RECT;
-            break;
-        case 'CIRCLE':
-            mode = ModeEnum.CIRCLE;
-            break;
-        case 'TRAIL':
-            mode = ModeEnum.TRAIL;
-            break;
-        case 'TRAILNOGRID':
-            mode = ModeEnum.TRAIL_NO_GRID;
-            break;
-        default:
-            mode = ModeEnum.RECT;
-            break;
-    }
-    Graphics.changeMode(mode);
-};
-
-var previouslyChecked = false;
-$(":radio").bind("change", function (event) {
-    if (automaticRadioButton.checked) {
-        Graphics.randomPaint();
-        // Listen to user's click - changing drawing procedure
-        canvas.removeEventListener("mousedown", Graphics.getCell, false);
-        canvas.addEventListener("mousedown", Graphics.getStartingPoint, false);
-        previouslyChecked = true;
-    }
-    else {
-        if (previouslyChecked) {
-            canvas.removeEventListener("mousedown", Graphics.getStartingPoint, false);
-            canvas.addEventListener("mousedown", Graphics.getCell, false);
-            previouslyChecked = false;
+/* START HOME > SPEED > SLIDER-SPEED */
+    // Initializing slider
+    $("#slider-speed").slider({
+        range: "min",
+        value: 90,
+        min: 1,
+        max: 100,
+        slide: function (event, ui) {
+            $("#input-speed").val(ui.value);
+        },
+        change: function (event, ui) {
+            var val = $(this).slider("value");
+            Life.changeSpeed(val);
         }
-    }
+    });
+    // Setting initial value for input "input-speed"
+    $("#input-speed").val(
+        function () {
+            var val = $("#slider-speed").slider("value");
+            Life.changeSpeed(val);
+            return val;
+    });
+    // Updating slider from input "input-speed"
+    $("#input-speed").keypress(
+    	function (e) {
+    	    var code = e.keyCode || e.which;
+    	    var val = $("#input-speed").val();
+    	    if (code === 13) {
+    	        $("#slider-speed").slider('value', val);
+    	        Life.changeSpeed(val);
+    	    }
+    	}
+    );
+    // Changing input background color
+    $("#input-speed").css({ 'background-color': '#eeeeee' });
+    $("#input-speed").focus(
+    function () {
+        $(this).css({ 'background-color': '#FFFFEEE' });
+    });
+    $("#input-speed").blur(
+    function () {
+        $(this).css({ 'background-color': '#eeeeee' });
+    });
+/* END HOME > SPEED > SLIDER-SPEED */
+
+/* START HOME > GRAPHICS > DISPLAY MODE */
+    $('#displayMode').change(function () {
+        var textMode = $(this).find(':selected').val();
+        var mode = ModeEnum.RECT;
+
+        switch (textMode) {
+            case 'RECT':
+                mode = ModeEnum.RECT;
+                break;
+            case 'CIRCLE':
+                mode = ModeEnum.CIRCLE;
+                break;
+            case 'TRAIL':
+                mode = ModeEnum.TRAIL;
+                break;
+            case 'TRAILNOGRID':
+                mode = ModeEnum.TRAIL_NO_GRID;
+                break;
+            default:
+                mode = ModeEnum.RECT;
+                break;
+        }
+        Graphics.changeMode(mode);
+    });
+/* STOP HOME >  GRAPHICS > DISPLAY MODE */
+
+/* START HOME > SELECTION MODE > CREATE GLIDER  */
+    var automaticRadioButton = document.getElementById("automaticRadioBtn");// Simple JS :-)
+    var previouslyChecked = false;
+    $(":radio").bind("change", function (event) {
+        if (automaticRadioButton.checked) {
+            Graphics.randomPaint();
+            // Listen to user's click - changing drawing procedure
+            canvas.removeEventListener("mousedown", Graphics.getCell, false);
+            canvas.addEventListener("mousedown", Graphics.getStartingPoint, false);
+            previouslyChecked = true;
+        }
+        else {
+            if (previouslyChecked) {
+                canvas.removeEventListener("mousedown", Graphics.getStartingPoint, false);
+                canvas.addEventListener("mousedown", Graphics.getCell, false);
+                previouslyChecked = false;
+            }
+        }
+    });
+/* STOP HOME > SELECTION MODE > CREATE GLIDER  */
 });
